edb0e9ec7c6893c310bcade22bc435ba84fbdb85
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-15 22:34:51 +0700

    build: adding react navigation package

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StyleSheet } from "react-native";
-import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "expo-status-bar";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
