cb301d2c8588daaa437a890fb619366fa4b68820
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-15 22:18:59 +0700

    style: displaying items in a grid

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Meals App!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
   );
 }
 
