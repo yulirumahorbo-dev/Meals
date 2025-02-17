af8c82a7edb0c63d763105369ef825d7b7b52111
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-17 23:10:50 +0700

    feat: displaying meals for each category

---
+++ components/MealItem.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function MealItem({ title }) {
+  return (
+    <View>
+      <Text>{title}</Text>
+    </View>
+  );
+}
+
+export default MealItem;
+
+const styles = StyleSheet.create({});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,7 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", { categoryId: itemData.item.id });
     }
     return (
       <CategoryGridTile
