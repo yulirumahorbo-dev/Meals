8b22feb4c0ef5490e9d497c2864701da6428c59e
Yuli Rumahorbo <yulirumahorbo.dev@gmail.com>
2025-02-17 22:42:33 +0700

    feat: implementing navigation between two screens

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
